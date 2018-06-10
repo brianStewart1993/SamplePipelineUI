ac75ac28d91f4d0ee275783d8cf932106e084fb0
jjnorth10 <jjnorthover@gmail.com>
2018-06-10 09:42:23 -0500

    Resolving jest test issues.

--- src/Tests/Login.test.js
+++ src/Tests/Login.test.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Login from '../Components/Login/Login';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -11,22 +13,22 @@ configure({ adapter: new Adapter() });
 
 describe('<Login>', function () {
   it('should have an input for the email', function () {
-    const wrapper = shallow(<Login/>);
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('#user-name')).to.have.length(1);
   });
 
   it('should have an input for the password', function () {
-    const wrapper = shallow(<Login/>);
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('#password')).to.have.length(1);
   });
 
   it('should have a button for login', function () {
-    const wrapper = shallow(<Login/>);
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('#login-btn')).to.have.length(1);
   });
 
   it('should have a button for sign up', function () {
-    const wrapper = shallow(<Login/>);
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('#sign-up')).to.have.length(1);
   });
 
